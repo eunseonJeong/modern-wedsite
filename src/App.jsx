5aeaed09a62c2911513c24ada50ed68ca010e1f0
정은선 <suni980215@gmail.com>
2024-06-12 14:34:32 +0900

    feat: 버튼추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Button from "./components/Button";
+import ButtonGradient from "./svg/ButtonGradient";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <div className="pt-[4.25rem] lg:pt-[5.25rem] overflow-hidden">
+    <Button className="mt-10" href='#login'>
+      Button
+    </Button>
+    </div>
+    <ButtonGradient />
+    </>
   )
 }
