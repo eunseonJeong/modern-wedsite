5a0762cfc57acde516bb9c59b534bdd943fff770
정은선 <139830751+rmsoft-sunny@users.noreply.github.com>
2024-06-12 15:00:44 +0900

    feat: 헤더 추가중

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import Button from "./components/Button";
-import ButtonGradient from "./svg/ButtonGradient";
+import ButtonGradient from "./img/svg/ButtonGradient";
 
 export default function App() {
-  return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <div className="pt-[4.25rem] lg:pt-[5.25rem] overflow-hidden">
-    <Button className="mt-10" href='#login'>
-      Button
-    </Button>
-    </div>
-    <ButtonGradient />
-    </>
-  )
-}
+    return (
+        <> 
+          <div className="pt-[4.25rem] lg:pt-[5.25rem] overflow-hidden">
+              <Button className="mt-10" href='#login'>
+                  Button
+              </Button>
+          </div>
+          <ButtonGradient/>
+      </>
+    )
+  }
